f14ce47dbbf0c6fd614e3e5ed7de398a1bf3feb9
SMDZ STUDIOS <netasgang@gmail.com>
2026-06-19 23:29:12 +0200

    Disable visitor webhook and redact URL
    Disable the visitor notification plugin (enabled set to false) and replace the configured webhookUrl with a placeholder to avoid exposing a secret. Other settings (locationApiUrl, sessionKey, locationTimeoutMs) remain unchanged.

--- plugins/visitor-config.js
+++ plugins/visitor-config.js
@@ -1,6 +1,6 @@
 window.SMDZ_VISITOR_CONFIG = {
-  enabled: true,
-  webhookUrl: 'https://discord.com/api/webhooks/1448225006747385867/Yr5jgOz13lPMBPi1WlM3r9OPUGxfBhVnqseZWKY3VhzggZ9d3dyFcmPGNIlKCPL0uoUW',
+  enabled: false,
+  webhookUrl: 'x',
   locationApiUrl: 'https://ipinfo.io/json',
   sessionKey: 'smdz_docs_visitor_notified_v3',
   locationTimeoutMs: 2500
